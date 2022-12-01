539ae69392f46888c150faaad8513e780d567117
Shivam Roy <royshivam214@gmail.com>
2022-12-01 20:12:12 +0530

    0.1

--- binarytree.c
+++ binarytree.c
@@ -11,7 +11,68 @@ struct node *createnode(int data)
 	n->left=n->right=NULL;
 	return n;
 }
+void insert(struct node *root,int data)
+{
+	struct node *nn=createnode(data);
+	struct queue *q=createqueue();
+	enqueue(q,root);
+	while(!isempty(q))
+	{
+		struct node *n=(struct node*)dequeue(q);
+		if(!n->left){n->left=nn;return;}
+		else if(!n->right){n->right=nn;return;}
+		if(n->left)enqueue(q,n->left);
+		if(n->right)enqueue(q,n->right);
+	}
+	delq(q);
+}
+void deln(struct node *root)
+{
+	
+}
+void pretraversal(struct node *root)
+{
+	printf("%d,",root->data);
+	if(root->left)pretraversal(root->left);
+	if(root->right)pretraversal(root->right);	
+}
+void posttraversal(struct node *root)
+{
+	if(root->left)posttraversal(root->left);
+	if(root->right)posttraversal(root->right);
+	printf("%d,",root->data);	
+}
+void intraversal(struct node *root)
+{
+	if(root->left)intraversal(root->left);
+	printf("%d,",root->data);
+	if(root->right)intraversal(root->right);
+}
+void delt(struct node *root)
+{
+	if(root->left)delt(root->left);
+	if(root->right)delt(root->right);
+	free(root);
+}
+void leveltraversal(struct node *root)
+{
+	struct queue *q=createqueue();
+	enqueue(q,root);
+	while(!isempty(q))
+	{
+		struct node *n=(struct node*)dequeue(q);
+		printf("%d,",n->data);
+		if(n->left)enqueue(q,n->left);
+		if(n->right)enqueue(q,n->right);
+	}
+	delq(q);
+}
 int main()
 {
+	struct node *root=createnode(1);
+	insert(root,2);
+	insert(root,3);
+	leveltraversal(root);
+	delt(root);
 	return 0;
 }
--- ll_queue.c
+++ ll_queue.c
@@ -2,5 +2,52 @@
 #include<stdlib.h>
 struct qnode
 {
-
+	void *data;
+	struct qnode *next;
 };
+struct queue{struct qnode *front,*rear;};
+struct qnode *createqnode(void *data)
+{
+	struct qnode *q=malloc(sizeof(struct qnode));
+	q->next=NULL;
+	q->data=data;
+	return q;
+}
+struct queue *createqueue()
+{
+	struct queue *q=malloc(sizeof(struct queue));
+	q->front=q->rear=NULL;
+	return q;
+}
+int isempty(struct queue *q){return q->front==NULL;}
+void enqueue(struct queue *q,void *data)
+{
+	struct qnode *qn=createqnode(data);
+	if(isempty(q))
+	{
+		q->front=q->rear=qn;
+		return;
+	}
+	q->rear->next=qn;
+	q->rear=qn;
+}
+void *dequeue(struct queue *q)
+{
+	if(isempty(q))return NULL;
+	struct qnode *qn=q->front;
+	q->front=qn->next;
+	void *data=qn->data;
+	free(qn);
+	return data;
+}
+void delq(struct queue *q)
+{
+	struct qnode *qn=q->front;
+	while(qn)
+	{
+		struct qnode *next=qn->next;
+		free(qn);
+		qn=next;
+	}
+	free(q);
+}
