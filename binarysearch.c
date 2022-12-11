517248f9c574e8c7bd0fccfcf970ca0c4e88c147
Shivam Roy <royshivam214@gmail.com>
2022-12-11 19:22:50 +0530

    0.1

--- binarysearch.c
+++ binarysearch.c
@@ -1,5 +1,5 @@
 #include"lib.c"
-//searches the middle element within a sorted list.
+//iterative
 int iter_search(int *arr,int s,int e,int sv)
 {
 	while(s<=e)
@@ -11,13 +11,22 @@ int iter_search(int *arr,int s,int e,int sv)
 	}
 	return -1;
 }
-int recur_search(int *arr,int s,int e,int sv){return -1;}
+//recursive
+int recur_search(int *arr,int s,int e,int sv)
+{
+	if(s>e)return -1;
+	int m=(s+e)/2;
+	if(sv==arr[m])return m;
+	else if(sv>arr[m])return recur_search(arr,m+1,e,sv);
+	else return recur_search(arr,s,m-1,sv);
+}
+//searches the middle element within a sorted list.
 int main()
 {
 	int n=6;
 	int arr[6]={3,5,6,7,8,9};
 	printarr(arr,n);
-	int i=iter_search(arr,0,n-1,3);
+	int i=recur_search(arr,0,n-1,6);//iter_search(arr,0,n-1,9);
 	if(i==-1)printf("searching failed!\n");
 	else printf("search value index:%d\n",i);
 	return 0;
